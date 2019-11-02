dfa8f8928aa4be4242c320d98ea9c636b519e13f
Joel Wang <workworkjoel@gmail.com>
2019-11-02 12:14:50 +0200

    added tests

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -11,13 +11,20 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class VarastoTest {
-
+    Varasto alkusaldovarasto;
+    Varasto alkusaldollinen;
     Varasto varasto;
+    Varasto kayttokelvoton;
+    Varasto kayttokelvotonAlkuSaldoTilavuus;
     double vertailuTarkkuus = 0.0001;
 
     @Before
     public void setUp() {
+        alkusaldovarasto = new Varasto(10,3);
         varasto = new Varasto(10);
+        kayttokelvoton = new Varasto(-1);
+        alkusaldollinen = new Varasto(10, -1);
+        kayttokelvotonAlkuSaldoTilavuus = new Varasto(-1,5);
     }
 
     @Test
@@ -65,4 +72,62 @@ public class VarastoTest {
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
 
+    @Test
+
+    public void varastoLiikaa(){
+        varasto.lisaaVarastoon(11);
+        assertEquals(0, varasto.paljonkoMahtuu(),vertailuTarkkuus);
+    }
+
+    @Test
+
+    public void lisaaVahemmanKuinNolla(){
+        varasto.lisaaVarastoon(-1);
+        assertEquals(10, varasto.paljonkoMahtuu(),vertailuTarkkuus);
+    }
+
+    @Test
+    
+    public void kayttokelvotonVarasto(){
+        assertEquals(0, kayttokelvoton.paljonkoMahtuu(),vertailuTarkkuus);
+    }
+
+    @Test
+
+    public void otaVahemmanKuinNolla(){
+        varasto.otaVarastosta(-1);
+        assertEquals(10, varasto.paljonkoMahtuu(),vertailuTarkkuus);
+    }
+
+    @Test
+
+    public void otaYliSaldo(){
+        varasto.lisaaVarastoon(4.0);
+        varasto.otaVarastosta(5.0);
+        assertEquals(10, varasto.paljonkoMahtuu(),vertailuTarkkuus);
+    }
+
+    @Test
+
+    public void kayttokelvotonAlkuSaldoVarasto(){
+        assertEquals(0, kayttokelvotonAlkuSaldoTilavuus.getTilavuus(),vertailuTarkkuus);
+    }
+
+    @Test
+
+    public void alkuSaldoAlleNolla(){
+        assertEquals(0, alkusaldollinen.getSaldo(),vertailuTarkkuus);
+    }
+
+    @Test
+
+    public void alkuSaldoToimii(){
+        assertEquals(7, alkusaldovarasto.paljonkoMahtuu(),vertailuTarkkuus);
+    }
+
+    @Test
+
+    public void toStringTesti(){
+        assertEquals("saldo = 3.0, vielä tilaa 7.0", alkusaldovarasto.toString());
+    }
 }
